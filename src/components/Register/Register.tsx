2b4859bb6acf960ab8e7ab376921a99c6f8541b4
thiagooo14 <thiagooo15@gmail.com>
2025-08-25 14:04:49 -0300

    style: adicionar estilo no Register

--- src/components/Register/Register.tsx
+++ src/components/Register/Register.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import type { RegisterCredentials } from '../../types';
 import { useAuth } from '../../hooks/useAuth';
 import { useNavigate } from 'react-router-dom';
+import './Register.scss'
 
 const Register = () => {
   const [credentials, setCredentials] = useState<RegisterCredentials>({
@@ -86,15 +87,21 @@ const Register = () => {
   };
 
   return (
-    <>
-      <div>
-        <p>Preencha os dados para se registrar</p>
+    <div className='register'>
+      <div className='register__header'>
+        <p className='register__header-text'>
+          Preencha os dados para se registrar
+        </p>
       </div>
+
       <div>
-        <form onSubmit={handleSubmit}>
-          <div className='form-group'>
-            <label htmlFor='name'>Nome Completo</label>
+        <form onSubmit={handleSubmit} className='register__form'>
+          <div className='register__form-group'>
+            <label htmlFor='name' className='register__form-label'>
+              Nome Completo
+            </label>
             <input
+              className='register__form-input'
               type='text'
               id='name'
               name='name'
@@ -106,9 +113,11 @@ const Register = () => {
               minLength={3}
             />
           </div>
-          <div className='form-group'>
-            <label htmlFor='email'>Email</label>
+
+          <div className='register__form-group'>
+            <label htmlFor='email' className='register__form-label'>Email</label>
             <input
+              className='register__form-input'
               type='email'
               id='email'
               name='email'
@@ -119,9 +128,11 @@ const Register = () => {
               disabled={isLoading}
             />
           </div>
-          <div className='form-group'>
-            <label htmlFor='password'>Senha</label>
+
+          <div className='register__form-group'>
+            <label htmlFor='password' className='register__form-label'>Senha</label>
             <input
+              className='register__form-input'
               type='password'
               id='password'
               name='password'
@@ -133,9 +144,11 @@ const Register = () => {
               minLength={6}
             />
           </div>
-          <div className='form-group'>
-            <label htmlFor='confirmPassword'>Confirmar Senha</label>
+
+          <div className='register__form-group'>
+            <label htmlFor='confirmPassword' className='register__form-label'>Confirmar Senha</label>
             <input
+              className='register__form-input'
               type='password'
               id='confirmPassword'
               name='confirmPassword'
@@ -147,23 +160,27 @@ const Register = () => {
               minLength={6}
             />
           </div>
-          <button type='submit' className='btn-register' disabled={isLoading}>
-            {isLoading ? (
-              <>
-                <div className='spinner-small'></div>
-                Criando conta...
-              </>
-            ) : (
-              <>
-                <i className='fas fa-user-plus'></i>
-                Criar Conta
-              </>
-            )}
-          </button>
+
+          <div className='register__btn-wrapper'>
+            <button type='submit' className='register__btn' disabled={isLoading}>
+              {isLoading ? (
+                <>
+                  <div className='register__btn-spinner'></div>
+                  Criando conta...
+                </>
+              ) : (
+                <>
+                  <i className='register__btn-signIn'></i>
+                  Criar Conta
+                </>
+              )}
+            </button>
+          </div>
+
           {error && <div className='error-message'>{error}</div>}
         </form>
       </div>
-    </>
+    </div>
   );
 };
 
